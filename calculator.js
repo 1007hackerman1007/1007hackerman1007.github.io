79f0ee52d0dbcd669e5bb141a11e7a49c9143ae4
1007hackerman1007 <1007hackerman1007@gmail.com>
2025-11-13 19:36:07 +0000

    feat: basic js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,22 @@
-fu
+var operations = document.getElementById("operations")
+var result = document.getElementById("result")
+
+function clickHandler(key) {
+    if (key === "=") {
+        calculate()
+    } else {
+        addSymbol(key)
+    }
+}
+
+function calculate() {
+    operation = operations.innerHTML
+    
+    console.log(operations.innerHTML)
+    result.innerHTML = eval(operations.innerHTML)
+    operations.innerHTML = ""
+}
+
+function addSymbol(key) {
+    operations.innerHTML += key
+}
